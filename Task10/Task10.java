5768d09d9fb8676899b028b389a8b9e669e7e275
AxelBankuna <axe.618@gmail.com>
2021-01-20 02:25:18 +0200

    fixed up task10

--- Task10/Task10.java
+++ Task10/Task10.java
@@ -6,12 +6,17 @@ public class Task10 {
     public static void commonLetters(String str1, String str2) {
         char[] charArray1 = str1.toLowerCase().toCharArray();
         char[] charArray2 = str2.toLowerCase().toCharArray();
+        boolean commaFlag = false;
 
+        System.out.print("Common letters: ");
         for (char letter1 : charArray1)
             for (char letter2 : charArray2)
                 if (letter1 == letter2) {
+                    if (commaFlag == true)
+                        System.out.print(", ");
                     System.out.print(letter1);
                     letter2 = '.';
+                    commaFlag = true;
                 }
         System.out.println();
     }
